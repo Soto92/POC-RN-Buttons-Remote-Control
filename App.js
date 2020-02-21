e75f5a827b0cda63e53ee3b60d2ede2294a2c575
soto92 <mauriciosoto@rede.ulbra.br>
2020-02-21 16:47:04 -0300

    implements imgs, logic & update readme

--- App.js
+++ App.js
@@ -1,18 +1,49 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
-  Text,
   StatusBar,
   TouchableOpacity,
   Dimensions,
+  Image,
 } from 'react-native';
-import {assassinsIcon, callOfDutyIcon, fifaIcon, lolIcon} from './src/svg/svgs';
+import {Ac, Cof, Nfs, Skyrim, Fifa, Fortnite, Pubg, Lol} from './src/svg/svgs';
 
 const App: () => React$Node = () => {
+  const [isActive, setActive] = useState([
+    {srcImg: require('./src/img/Ac.jpg'), state: false, name: 'Ac'},
+    {srcImg: require('./src/img/Cof.jpg'), state: false, name: 'Cof'},
+    {srcImg: require('./src/img/nfs.jpg'), state: false, name: 'Nfs'},
+    {srcImg: require('./src/img/skyrim.jpg'), state: false, name: 'Skyrim'},
+    {srcImg: require('./src/img/fifa.jpg'), state: false, name: 'Fifa'},
+    {srcImg: require('./src/img/fortnite.jpg'), state: false, name: 'Fortnite'},
+    {srcImg: require('./src/img/pubg3.jpg'), state: false, name: 'Pubg'},
+    {srcImg: require('./src/img/lol.jpeg'), state: false, name: 'Lol'},
+  ]);
+
+  const [srcImage, setSrcImage] = useState();
+
+  const activeButton = value => {
+    const eX = isActive.map(item => {
+      if (item.name === value) {
+        return {...item, state: !item.state};
+      } else {
+        return {...item, state: false};
+      }
+    });
+    setActive(eX);
+    eX.forEach(item => {
+      if (item.state && item.name === value) {
+        setSrcImage(item.srcImg);
+      } else if (!item.state && item.name === value) {
+        setSrcImage(null);
+      }
+    });
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -20,12 +51,15 @@ const App: () => React$Node = () => {
         <ScrollView
           contentContainerStyle={style.content}
           contentInsetAdjustmentBehavior="automatic">
+          <View style={style.viewImg}>
+            {srcImage && <Image style={style.img} source={srcImage} />}
+          </View>
           <View style={style.borderbuttons}>
             <View style={style.buttongroup}>
               <TouchableOpacity
                 activeOpacity={1}
                 style={{
-                  backgroundColor: '#424242',
+                  backgroundColor: isActive[2].state ? '#353535' : '#424242',
                   borderColor: 'black',
                   borderWidth: 1,
                   width: 50,
@@ -34,13 +68,13 @@ const App: () => React$Node = () => {
                   justifyContent: 'center',
                   alignItems: 'center',
                 }}
-                onPress={() => console.log('press')}>
-                <Text>0</Text>
+                onPress={() => activeButton('Nfs')}>
+                <Nfs color={isActive[2].state ? '#53FFFB' : 'white'} />
               </TouchableOpacity>
               <TouchableOpacity
                 activeOpacity={1}
                 style={{
-                  backgroundColor: '#424242',
+                  backgroundColor: isActive[0].state ? '#353535' : '#424242',
                   borderColor: 'black',
                   borderWidth: 1,
                   width: 50,
@@ -49,13 +83,13 @@ const App: () => React$Node = () => {
                   justifyContent: 'center',
                   alignItems: 'center',
                 }}
-                onPress={() => console.log('press')}>
-                <Text>0</Text>
+                onPress={() => activeButton('Ac')}>
+                <Ac color={isActive[0].state ? '#53FFFB' : 'white'} />
               </TouchableOpacity>
               <TouchableOpacity
                 activeOpacity={1}
                 style={{
-                  backgroundColor: '#424242',
+                  backgroundColor: isActive[3].state ? '#353535' : '#424242',
                   borderColor: 'black',
                   borderWidth: 1,
                   width: 50,
@@ -64,13 +98,13 @@ const App: () => React$Node = () => {
                   justifyContent: 'center',
                   alignItems: 'center',
                 }}
-                onPress={() => console.log('red')}>
-                <Text>0</Text>
+                onPress={() => activeButton('Skyrim')}>
+                <Skyrim color={isActive[3].state ? '#53FFFB' : 'white'} />
               </TouchableOpacity>
               <TouchableOpacity
                 activeOpacity={1}
                 style={{
-                  backgroundColor: '#424242',
+                  backgroundColor: isActive[6].state ? '#353535' : '#424242',
                   borderColor: 'black',
                   borderWidth: 1,
                   width: 50,
@@ -79,14 +113,14 @@ const App: () => React$Node = () => {
                   justifyContent: 'center',
                   alignItems: 'center',
                 }}
-                onPress={() => console.log('blue')}>
-                <Text>0</Text>
+                onPress={() => activeButton('Pubg')}>
+                <Pubg color={isActive[6].state ? '#53FFFB' : 'white'} />
               </TouchableOpacity>
               <TouchableOpacity
                 activeOpacity={1}
                 style={{
                   position: 'absolute',
-                  backgroundColor: '#424242',
+                  backgroundColor: isActive[1].state ? '#353535' : '#424242',
                   borderColor: 'black',
                   borderWidth: 1,
                   width: 58,
@@ -95,8 +129,8 @@ const App: () => React$Node = () => {
                   justifyContent: 'center',
                   alignItems: 'center',
                 }}
-                onPress={() => console.log('blue')}>
-                {assassinsIcon}
+                onPress={() => activeButton('Cof')}>
+                <Cof color={isActive[1].state ? '#53FFFB' : 'white'} />
               </TouchableOpacity>
             </View>
             <View style={style.bottomButons}>
@@ -104,32 +138,35 @@ const App: () => React$Node = () => {
                 activeOpacity={1}
                 style={{
                   ...style.bottomButon,
+                  backgroundColor: isActive[4].state ? '#353535' : '#424242',
                   width: 150,
                   justifyContent: 'flex-start',
                   paddingTop: 3,
                   borderTopEndRadius: 40,
                   borderTopStartRadius: 40,
                 }}
-                onPress={() => console.log('blue')}>
-                {callOfDutyIcon}
+                onPress={() => activeButton('Fifa')}>
+                <Fifa color={isActive[4].state ? '#53FFFB' : 'white'} />
               </TouchableOpacity>
               <TouchableOpacity
                 activeOpacity={1}
                 style={{
                   ...style.bottomButon,
+                  backgroundColor: isActive[5].state ? '#353535' : '#424242',
                   borderBottomStartRadius: 40,
                 }}
-                onPress={() => console.log('blue')}>
-                {lolIcon}
+                onPress={() => activeButton('Fortnite')}>
+                <Fortnite color={isActive[5].state ? '#53FFFB' : 'white'} />
               </TouchableOpacity>
               <TouchableOpacity
                 activeOpacity={1}
                 style={{
                   ...style.bottomButon,
+                  backgroundColor: isActive[7].state ? '#353535' : '#424242',
                   borderBottomEndRadius: 40,
                 }}
-                onPress={() => console.log('blue')}>
-                {fifaIcon}
+                onPress={() => activeButton('Lol')}>
+                <Lol color={isActive[7].state ? '#53FFFB' : 'white'} />
               </TouchableOpacity>
             </View>
           </View>
@@ -145,7 +182,7 @@ const {width, height} = Dimensions.get('window');
 
 const style = StyleSheet.create({
   content: {
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     height: height,
   },
@@ -190,6 +227,17 @@ const style = StyleSheet.create({
     alignItems: 'center',
     borderColor: 'black',
     borderWidth: 0.5,
-    backgroundColor: '#424242',
+  },
+  viewImg: {
+    width: width - 48,
+    height: width - 200,
+    marginVertical: 30,
+    justifyContent: 'center',
+  },
+  img: {
+    width: width - 48,
+    height: width - 200,
+    borderRadius: 30,
+    marginVertical: 30,
   },
 });
--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # POC-RN-Buttons-Remote-Control
 
-Implementado um "Button Group" baseado num de controle remoto.
+## Description:
+
+ Implement a group of buttons based on the remote control. In this application, one button disables the other, each button renders an image of a game corresponding to its button.
+
+Use:
+```
+Node: "12,14,01",
+react: "16.9.0",
+react-native: "0.61.5",
+react-native-svg: "^11.0.1"
+
+```
+
+How run:
+
+```
+npm install
+```
+
+Connect device &
+
+```
+react-native run-android  //(if android)
+```
+
+```
+npm start
+```
+
+## Screenshot demo:
+
+![First Screen](/src/img/demo.gif)
+
+
+By Mauricio Soto
